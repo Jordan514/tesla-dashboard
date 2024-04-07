927fc15103f160eecaf07834a860e48a773e033a
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-04-07 01:29:14 -0500

    Add csv and fullscreen button

---
+++ script.js
@@ -0,0 +1,5 @@
+let documentBody = document.querySelector("body");
+let documentHeader = documentBody.querySelector("#header");
+let documentContent = documentBody.querySelector("#content");
+let list = documentContent.querySelector("ul");
+let arrayOfLinks = ["https://www.youtube.com", "https://www.netflix.com"];
