4525e4f8095f4afcab675032adf54e41821127c0
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-04-07 01:42:06 -0500

    dont remember

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let documentBody = document.querySelector("body");
-let documentHeader = documentBody.querySelector("#header");
-let documentContent = documentBody.querySelector("#content");
+let documentHeader = document.querySelector("#header");
+let documentContent = document.querySelector("#content");
 let list = documentContent.querySelector("ul");
+
 let arrayOfLinks = ["https://www.youtube.com", "https://www.netflix.com"];
