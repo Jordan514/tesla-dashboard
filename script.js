4e829a14b25124423d1a68ee1c21323b9cc7be2f
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-04-08 14:32:15 -0500

    Add links in list change from csv to json for data

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let documentBody = document.querySelector("body");
 let documentHeader = document.querySelector("#header");
 let documentContent = document.querySelector("#content");
-let list = documentContent.querySelector("ul");
+let documentList = documentContent.querySelector("ul");
 
-let arrayOfLinks = ["https://www.youtube.com", "https://www.netflix.com"];
+let arrayOfLinks = [
+  "https://www.youtube.com",
+  "https://www.netflix.com",
+  "https://www.max.com",
+];
+for (url of arrayOfLinks) {
+  let listItem = document.createElement("li");
+  let link = document.createElement("a");
+  link.setAttribute("href", url);
+  link.textContent = url; //replace with name once objects complete
+  listItem.appendChild(link);
+  documentList.appendChild(listItem);
+}
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
