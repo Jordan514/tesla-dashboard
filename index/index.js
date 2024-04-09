4094689d359ec7cec836f2e738068a51d53fe35f
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-04-08 19:17:58 -0500

    Sort into folders and add images to links

--- index/index.js
+++ index/index.js
@@ -4,19 +4,22 @@ let documentContent = document.querySelector("#content");
 let documentList = documentContent.querySelector("ul");
 
 async function createLinks() {
-  let json = (await fetch("./data.json"))
+  let data = (await fetch("./index/data.json"))
     .json()
     .then((json) => Object.entries(json));
 
-  for (obj of await json) {
+  for (obj of await data) {
     let array = obj[1];
     let listItem = document.createElement("li");
     let link = document.createElement("a");
+    let img = document.createElement("img");
+
+    img.setAttribute("src", array["image_url"]);
     link.setAttribute("href", array["url"]);
-    link.textContent = array["name"]; //replace with name once objects complete
+
+    link.appendChild(img);
     listItem.appendChild(link);
     documentList.appendChild(listItem);
-    console.log(array["name"]);
   }
 }
 
